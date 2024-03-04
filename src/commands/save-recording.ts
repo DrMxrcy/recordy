18bf0ed5e353be01311b7f70467d7c7cd0a54a70
DrMxrcy <58747968+DrMxrcy@users.noreply.github.com>
2024-03-04 01:34:28 -0500

    Update save-recording.ts

--- src/commands/save-recording.ts
+++ src/commands/save-recording.ts
@@ -2,7 +2,6 @@ import { Command } from '../utils/appCommands';
 import { APIApplicationCommandOptionChoice, SlashCommandBuilder } from 'discord.js';
 import { voiceRecorder } from '../utils/voice';
 import { AudioExportType } from '@kirdock/discordjs-voice-recorder/lib/models/types';
-import { envs } from '../utils/environment';
 
 type Choices = APIApplicationCommandOptionChoice & {value: AudioExportType};
 const choices: Choices[] = [
@@ -13,50 +12,63 @@ const choices: Choices[] = [
 const command: Command = {
     data: new SlashCommandBuilder()
         .setName('save')
-        .setDescription(`Save the last x minutes ( Max 60 minutes)`)
+        .setDescription(`Save the last x minutes (Max 60 minutes)`)
         .addIntegerOption(option =>
             option
                 .setName('minutes')
-                .setDescription('How many minutes should be saved ( Max 60 minutes)')
+                .setDescription('How many minutes should be saved (Max 60 minutes)')
                 .setMinValue(1)
                 .setMaxValue(60)
         )
-        .addStringOption((option) =>
-            option.setName('type').setDescription('save as single file or as zip file with a file per user').setChoices(...choices)
+        .addStringOption(option =>
+            option
+                .setName('type')
+                .setDescription('Save as single file or as zip file with a file per user')
+                .setChoices(...choices)
+        )
+        .addStringOption(option =>
+            option
+                .setName('customname')
+                .setDescription('Custom name for the saved file (special characters will be removed)')
         )
         .toJSON(),
-    async execute (interaction) {
-        if(!interaction.guildId) {
+    async execute(interaction) {
+        if (!interaction.guildId) {
             return 'No guild provided';
         }
 
         const guild = interaction.inCachedGuild() ? await interaction.guild.fetch() : interaction.guild;
-        if(!guild) {
+        if (!guild) {
             return 'Guild cannot be fetched';
         }
 
         await interaction.deferReply();
-        const minutes = interaction.options.getInteger('minutes');
-        const exportType = (interaction.options.getString('type') as AudioExportType | null) ?? undefined;
-        const buffer = await voiceRecorder.getRecordedVoiceAsBuffer(interaction.guildId, exportType ?? undefined, minutes ?? undefined);
-        const date = new Date().toISOString();
+        const minutes = interaction.options.getInteger('minutes') ?? 1;
+        const exportType = interaction.options.getString('type') as AudioExportType | null ?? 'single';
+        let customNameInput = interaction.options.getString('customname');
+        customNameInput = customNameInput ? customNameInput.replace(/[^a-zA-Z0-9-_]/g, '_') : '';
+
+        const date = new Date().toISOString().replace(/[:.-]/g, '_'); //
+
+        const baseFileName = `${customNameInput}${customNameInput ? '-' : ''}Recording_${date}`;
 
         let fileType: string, fileName: string;
         if (exportType === 'single') {
             fileType = 'audio/mp3';
-            fileName = `${date}.mp3`;
-
+            fileName = `${baseFileName}.mp3`;
         } else {
             fileType = 'application/zip';
-            fileName = `${date}-all-streams.zip`;
+            fileName = `${baseFileName}-all-streams.zip`;
         }
 
+        const buffer = await voiceRecorder.getRecordedVoiceAsBuffer(interaction.guildId, exportType, minutes);
+
         await interaction.editReply({
-            files: [ {
+            files: [{
                 attachment: buffer,
                 contentType: fileType,
                 name: fileName,
-            } ],
+            }],
         });
     },
 };
